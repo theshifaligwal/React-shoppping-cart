dd96dd16cdc73d7bb6a4520c0b8314614ce49372
theshifaligwal <gwal.shifali2223@gmail.com>
2021-08-08 14:23:34 +0530

    Store everything

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import "react-toastify/dist/ReactToastify.css";
 
 import "./App.css";
 import { toast } from "react-toastify";
+import BuyPage from "./Components/BuyPage";
 
 function App() {
   const [cartItem, setCartItem] = useState([]);
@@ -35,20 +36,8 @@ function App() {
 
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    
+      <BuyPage addInCart={addInCart} />
     </div>
   );
 }
--- src/Components/BuyPage.js
+++ src/Components/BuyPage.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import { Axios } from "axios";
+import Axios  from "axios";
 
 import { random, commerce } from "faker";
 import { Container, Col, Row } from "reactstrap";
@@ -19,13 +19,34 @@ const BuyPage = ({ addInCart }) => {
 
   const fetchPhotos = async () => {
     const { data } = await Axios.get(localurl);
-  };
 
-  const { photos } = data;
+    const { photos } = data;
+
+    const allProduct = photos.map((photo) => ({
+      smallImage: photo.setProduct.medium,
+      tinyImage: photo.setProduct.tiny,
+      productName: random.word(),
+      productPrice: commerce.price(),
+      id: random.uuid(),
+    }));
+
+    setProduct(allProduct);
+  };
 
   useEffect(() => {
     fetchPhotos();
   }, []);
 };
+
+return (
+  <Container fluid>
+    <h1 className="text-success text-center">Buy Page</h1>
+    <Row>
+      {product.map((product) => {
+        <span key={product.id}>{product.productName}</span>;
+      })}
+    </Row>
+  </Container>
+);
 
 export default BuyPage;
